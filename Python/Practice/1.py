be753001f3acc540630f444130ff2adf01031124
Akshay Sharma <officialakshay1896@gmail.com>
2024-09-09 16:34:02 +0530

    08-09-24 Practice

--- Python/Practice/1.py
+++ Python/Practice/1.py
@@ -18,13 +18,35 @@
 
 #LIST
 
-ls = [2,3,4.5,"Hello", True, [2,4]]
-print(ls[-1])
-print(type(ls[-1]))
+# ls = [2,3,4.5,"Hello", True, [2,4]]
+# print(ls[-1])
+# print(type(ls[-1]))
 # a = ls[-1]
 # print(a[1])
 
+# 5 Write a Python program that calls an external command with different text color and style
 
+import subprocess
+from colorama import Fore,Style,init
+
+init()
+
+command = ["cmd","/c","dir"]
+
+try:
+    result = subprocess.run(command, capture_output=True, text = True, check= True)
+    output = result.stdout.splitlines()
+    print("Command Ouput: ")
+    for line in output:
+        if "Directory of" in line:
+            print(Fore.LIGHTGREEN_EX + line + Style.RESET_ALL)
+        elif "<DIR>" in line:
+            print(Fore.MAGENTA + line + Style.RESET_ALL)
+        else:
+            print(Fore.WHITE + line + Style.RESET_ALL)
+        
+except subprocess.CalledProcessError as e:
+    print(f"Error while running the command: {e}")
 
 
 
